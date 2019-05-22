f139a7fef9db1f5e403ef9437266369dd2a856a5
신우진 <voltsoftdev@gmail.com>
2019-05-22 14:53:27 +0900

    1주차 샘플앱 수정

---
+++ sampleApp/app/src/main/java/com/dev/voltsoft/sampleapp/PageRegistration.java
@@ -0,0 +1,16 @@
+package com.dev.voltsoft.sampleapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PageRegistration extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.page_registration);
+
+    }
+}
